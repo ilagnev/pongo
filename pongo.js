29035a6788844ebf731ff6badb1f816e24afc3e2
skate <ilagnev@gmail.com>
2016-10-28 01:47:42 +0300

    move the ball, actual rectangle ⏹

--- pongo.js
+++ pongo.js
@@ -10,8 +10,8 @@ class Pos
 
 class Rect 
 {
-	constructor(width, height) {
-		this.pos = new Pos();
+	constructor(offsetLeft, offsetTop, width, height) {
+		this.pos = new Pos(offsetLeft, offsetTop);
 		this.size = new Pos(width, height);
 	}
 }
@@ -19,21 +19,58 @@ class Rect
 class Ball extends Rect 
 {
 	constructor() {
-		super(10, 10);
-		this.vel = new Pos();
+		super(
+			(canvas.width / 2) -5, 
+			(canvas.height / 2) -5,
+			10, 
+			10
+		);
+		// velocity: pixels per sec
+		this.vel = {x:0, y:0};
 	}
 }
-
-var ball = new Ball();
-ball.pos.x = 111;
-ball.pos.y = 111;
-console.log(ball);
 
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-context.fillStyle = '#551A8B';
-context.fillRect(0, 0, canvas.width, canvas.height);
+var ball = new Ball();
+// set ball velocity in pixels per sec
+ball.vel.x = 333 * (Math.random() > .5 ? 1 : -1);
+ball.vel.y = 333 * (Math.random() > .5 ? 1: -1);
+console.log(ball);
 
-context.fillStyle = '#fff';
-context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+var prevUpdatedTime;
+function frameUpdated(ms) {
+	if (prevUpdatedTime) {
+		// delta between frames
+		update((ms - prevUpdatedTime) / 1000);
+	}
+	prevUpdatedTime = ms;
+	requestAnimationFrame(frameUpdated);
+}
+
+function update(delta) {
+	// detect collision with walls and change direction of velocity
+	ball.vel.x = ball.pos.x + ball.size.x > canvas.width || ball.pos.x < 0 
+		? -ball.vel.x 
+		: ball.vel.x;
+		
+	ball.vel.y = ball.pos.y + ball.size.y > canvas.height || ball.pos.y < 0
+		? -ball.vel.y 
+		: ball.vel.y;
+
+	// change ball position in time with deltatime
+	ball.pos.x += ball.vel.x * delta;
+	ball.pos.y += ball.vel.y * delta;
+
+	// draw background
+	context.fillStyle = '#551A8B';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	// draw ball
+	context.fillStyle = '#fff';
+	context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+}
+
+// run and slide
+frameUpdated();
